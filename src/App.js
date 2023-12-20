ffc37b10ccaf11b107e1afadcea3b8eb3d1d9694
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-12-20 16:59:11 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Header} from './Components/Header';
 import {Footer} from './Components/Footer';
-import {Future} from './Components/Future';
+// import {Future} from './Components/Future';
 import {HeroSection} from './Components/HeroSection';
 import {Section2} from './Components/Section2';
 import { Route, Routes } from 'react-router-dom';
@@ -13,7 +13,7 @@ function App() {
       <Routes>  
       <Route path={"/"} element = {<HeroSection/>}/>
       <Section2 />
-      <Future />
+      {/* <Future /> */}
       </Routes>  
        <Footer />
   
