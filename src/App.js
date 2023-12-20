06c56df4e315d9885bd8d1aba84b2c385c1c0c42
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-12-20 15:59:14 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Header} from './Components/Header';
 import {Footer} from './Components/Footer';
 import Future from './Components/Future';
-import HeroSection from './Components/HeroSection';
+import {HeroSection} from './Components/HeroSection';
 import Section2 from './Components/Section2';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,13 +1,26 @@
-// Example: Header.js
-
-import React from 'react';
-
-const HeroSection = () => {
-  return (
-    <header>
-      <h1>Hero Section2</h1>
-    </header>
-  );
-};
-
-export default HeroSection;
+import React from "react";
+import '../assets/style/hero.css';
+export const HeroSection = () => {
+    return(
+        <>
+            <div className="container-fluid myhero">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1 style={{fontSize:"100px"}} className="text-center text-white">Web3Makr Redefining</h1>
+                            <h4  style={{fontSize:"60px"}} className="text-center text-white">How You Create</h4>
+                            <h6  style={{fontSize:"40px"}} className="text-center text-white">Unlock the blockchain potential to create Web3 Applications</h6>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <button class="btn-demo">
+                                {/* <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PlayCircleFilledWhiteIcon" style={{color: "white"}}><path transform="scale(0.5, 0.5)" d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path></svg> */}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
